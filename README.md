0aae1bdb6627bc8c99cc02a0806995b2be1d1cdd
MARIKO ALVARADO <marikoaalvarado@gmail.com>
2021-01-18 22:13:12 -0800

    hooks

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# To-Do
+# LAB - TO-DO
+
+## Project: 31
+
+### Author: Mariko Alvarado
+
+### Links and Resources
+
+-  Ricardo, Dina, Nathan, Simon
+
+#### UML
+
+![uml](UML.png)
---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ToDo from './components/todo/todo-connected';
+
+const App = () => {
+
+  return (
+    <>
+      <ToDo />
+    </>
+  );
+}
+export default App;
